bb48d3fcf5c7d99a32603a9adbfdd5371ddb720d
Kaique da Silva <kaeyosthaeron@gmail.com>
2019-08-27 16:03:57 -0300

    Present fixing all package

--- index.js
+++ index.js
@@ -1,29 +1,2 @@
-const isTTY = process.stdin.isTTY;
-const isPipe = !isTTY;
-const { Transform } = require('stream');
-const args = process.argv.slice(2);
-const searchData = new Transform({
-    transform(chunk, encoding, callback) {
-        let raw = chunk.toString();
-        let presence = args.filter(v => {
-            if (findFn(v, raw)) {
-                return v;
-            }
-        });
-        if (presence.length) {
-            this.push(Buffer.from('Present: ' + presence.join(' ') + '\n'));
-        } else {
-            this.push(Buffer.from('Unpresent terms!\n'));
-        }
-
-        callback();
-    }
-})
-
-if (isPipe) {
-    process.stdin.pipe(searchData).pipe(process.stdout);
-    process.stdin.resume();
-}
-
 let findFn = (v, content) => content.indexOf(v) > -1;
 exports.find = findFn;
