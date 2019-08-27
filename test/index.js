76eb060950bc2104169d75374a138d9d7277ec7b
Kaique da Silva <kaeyosthaeron@gmail.com>
2019-08-27 16:54:53 -0300

    Testing the out case of text search

--- test/index.js
+++ test/index.js
@@ -5,4 +5,8 @@ describe('Nap text searcher', () => {
     it('find text in content', () => {
         assert.ok(nap.find('love', 'i love grapes'), 'failing on find content')
     });
+
+    it('find not the text in content', () => {
+        assert.ok(!nap.find('orange', 'i love grapes', 'failing to be tolerant'))
+    });
 });
