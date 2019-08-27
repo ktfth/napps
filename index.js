217ab976314bbe04f3a27632e75b51d7fe03aa25
Kaique da Silva <kaeyosthaeron@gmail.com>
2019-08-27 15:38:49 -0300

    Starting a project for text search

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+const isPipe = !Boolean(process.stdin.isTTY);
+const { Transform } = require('stream');
+const args = process.argv.slice(2);
+const searchData = new Transform({
+    transform(chunk, encoding, callback) {
+        let raw = chunk.toString();
+        let presence = args.filter(v => {
+            if (raw.indexOf(v) > -1) {
+                return v;
+            }
+        });
+        if (presence.length) {
+            this.push(Buffer.from('Present: ' + presence.join(' ') + '\n'));
+        } else {
+            this.push(Buffer.from('Unpresent terms!\n'));
+        }
+
+        callback();
+    }
+})
+
+if (isPipe) {
+    process.stdin.pipe(searchData).pipe(process.stdout);
+    process.stdin.resume();
+}
+
+let findFn = (v, content) => content.indeOf(v) > -1;
+exports.find = findFn;
---
+++ test/index.js
@@ -0,0 +1,9 @@
+'use strict';
+const assert = require('assert');
+const nap = require('../');
+
+describe('Nap text searcher', () => {
+    it('find text in content', () => {
+        assert.ok(nap.find('love', 'i love grapes'), 'failing on find content')
+    });
+});
