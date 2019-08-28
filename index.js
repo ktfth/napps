693e93af229ce9962773235672f3bb5f359991f1
Kaique da Silva <kaeyosthaeron@gmail.com>
2019-08-28 08:08:07 -0300

    Presence count of method

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let findFn = (v, content) => content.indexOf(v) > -1;
 exports.find = findFn;
+
+let countFn = (v, content) => {
+    let out = 0;
+    out = content.match(new RegExp(v, 'ig')).length;
+    return out;
+};
+exports.count = countFn;
--- test/index.js
+++ test/index.js
@@ -9,4 +9,8 @@ describe('Nap text searcher', () => {
     it('find not the text in content', () => {
         assert.ok(!nap.find('orange', 'i love grapes', 'failing to be tolerant'))
     });
+
+    it('count some times the content appears', () => {
+        assert.equal(nap.count('sample', 'sample some sample'), 2);
+    });
 });
