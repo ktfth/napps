fca756e58f5da65d114c792ea814ead198a3433f
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-08-21 18:26:30 -0300

    Subject and Observers
    Included tests for those instances

--- index.js
+++ index.js
@@ -3,6 +3,49 @@ const { JSDOM } = jsdom;
 const { Transform } = require('stream');
 const jquery = require('jquery');
 
+class Subject {
+  constructor() {
+    this.observers = [];
+    this.state = 0;
+  }
+
+  notifyAllObservers() {
+    this.observers.forEach(o => {
+      let self = this;
+      o.update();
+      return this;
+    });
+    return this;
+  }
+
+  getState() { return this.state; }
+
+  setState(state) {
+    this.state = state;
+    this.notifyAllObservers();
+    return this;
+  }
+
+  attach(observer) {
+    this.observers.push(observer);
+    return this;
+  }
+}
+exports.Subject = Subject;
+
+class Observer {
+  constructor() {
+    this.subject = new Subject();
+    this.subject.attach(this);
+  }
+
+  update(fn=function () {}, ...args) {
+    fn.apply(this, args);
+    return this;
+  }
+}
+exports.Observer = Observer;
+
 class Agent {
   constructor(content) {
     this.content = content;
--- test/index.js
+++ test/index.js
@@ -5,6 +5,52 @@ const assert = require('assert');
 
 const { Transform } = require('stream');
 
+describe('Nap Subject', () => {
+  it('should be the instance', () => {
+    let subject = new nap.Subject();
+    assert.ok(subject instanceof nap.Subject);
+  });
+
+  it('should have observers', () => {
+    let subject = new nap.Subject();
+    assert.deepEqual(subject.observers, []);
+  });
+
+  it('should have state', () => {
+    let subject = new nap.Subject();
+    assert.equal(subject.state, 0);
+  });
+
+  it('should have get state', () => {
+    let subject = new nap.Subject();
+    assert.equal(subject.getState(), 0);
+  });
+
+  it('should have set state', () => {
+    let subject = new nap.Subject();
+    assert.ok(subject.setState(1) instanceof nap.Subject);
+    assert.equal(subject.getState(), 1);
+  });
+});
+
+describe('Nap Observer', () => {
+  it('should be the instance', () => {
+    let observer = new nap.Observer();
+    assert.ok(observer instanceof nap.Observer);
+  });
+
+  it('should be the instance of Subject', () => {
+    let subject = new nap.Subject();
+    let observer = new nap.Observer();
+    assert.ok(subject.attach(observer) instanceof nap.Subject);
+  });
+
+  it('should have update', () => {
+    let observer = new nap.Observer();
+    assert.ok(observer.update((a, b) => a + b, 1, 2) instanceof nap.Observer);
+  });
+});
+
 describe('Nap Agent', () => {
   it('should be an instance of described', () => {
     let actual = new nap.Agent();
